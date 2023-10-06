35d31b31a5792404a6ca0989eb04266c562f6cc0
Ishikawa017 <onoborisalmon@gmail.com>
2023-10-06 23:23:43 +0900

    test

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -4,15 +4,6 @@ import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     ssr: true,
-    icons: {
-      defaultSet: "mdi",
-      aliases: {
-        ...aliases,
-      },
-      sets: {
-        mdi,
-      },
-    },
   });
 
   nuxtApp.vueApp.use(vuetify);
