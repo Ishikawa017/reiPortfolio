5ab3fa5b8d5bfab94e5a566de79d6e8b80299a00
Ishikawa017 <onoborisalmon@gmail.com>
2023-10-06 22:56:42 +0900

    carousel追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,28 @@
+import { defineNuxtConfig } from "nuxt/config";
+import vuetify from "vite-plugin-vuetify";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  typescript: {
+    shim: false,
+    strict: true,
+    typeCheck: true,
+  },
+  css: ["vuetify/styles"],
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    ssr: {
+      noExternal: ["vuetify"],
+    },
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
+  hooks: {
+    "vite:extendConfig": (config) => {
+      config.plugins!.push(vuetify());
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <!-- ファーストビュー -->
+  <!-- カルーセル -->
+
+  <v-carousel
+    height="100vh"
+    cycle
+    :show-arrows="false"
+    hide-delimiter-background
+    hide-delimiters
+  >
+    <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src">
+      <v-parallax scale="0.1">
+        <v-img
+          eager
+          height="100vh"
+          cover
+          :src="item.src"
+          gradient="to top, rgba(0,0,0,0.2),rgba(0,0,0,0.6)"
+        >
+        </v-img>
+      </v-parallax>
+    </v-carousel-item>
+  </v-carousel>
+
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+  <p>test</p>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      items: [
+        {
+          src: "/images/carousel/img1.png",
+        },
+        {
+          src: "/images/carousel/img2.png",
+        },
+        {
+          src: "/images/carousel/img3.png",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
