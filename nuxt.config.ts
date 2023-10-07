3c25a651a648919ee2dbff188bd2a7f614891603
Ishikawa017 <onoborisalmon@gmail.com>
2023-10-07 16:08:28 +0900

    config修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,27 @@ import { defineNuxtConfig } from "nuxt/config";
 import vuetify from "vite-plugin-vuetify";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Ishikawa's Portfolio",
+      htmlAttrs: {
+        lang: "ja",
+      },
+      meta: [
+        {
+          property: "og:url",
+          content: "https://ishikawa017.github.io/reiPortfolio/",
+        },
+        { property: "og:title", content: "Ishikawa's Portfolio" },
+        {
+          property: "og:site:name",
+          content: "Ishikawa's Portfolio",
+        },
+      ],
+    },
+  },
   typescript: {
     shim: false,
     strict: true,
