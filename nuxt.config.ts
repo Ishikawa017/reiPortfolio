36354f64f1567f3ad3424529982052fd5022cdfa
Ishikawa017 <onoborisalmon@gmail.com>
2023-10-07 16:54:55 +0900

    config修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ import { defineNuxtConfig } from "nuxt/config";
 import vuetify from "vite-plugin-vuetify";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  target: "static",
   app: {
     baseURL: "/reiPortfolio/",
     cdnURL: "/reiPortfolio/",
